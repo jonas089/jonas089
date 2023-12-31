65fd0afa4e72c62216176bfc73c8c123721d2183
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-12-31 23:25:37 +0100

    add section for articles and change styples

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline';
 import casper from '../assets/casper.png';
 import profile from '../assets/profile.jpeg';
 import github from '../assets/github.png';
@@ -35,18 +35,7 @@ const navigation = {
 }
 
 
-
-
-const repos = [
-  { name: 'Casper-Circom', description: 'Zero Knowledge: Circom verifier integrated with the Casper Node.', href: 'https://github.com/jonas089/casper-circom', icon: github },
-  { name: 'Pos-Rust', description: 'Proof of stake in Rust.', href: 'https://github.com/jonas089/pos-rust', icon: github },
-  { name: 'Macro Compiler', description: 'Smart Contract Metadata emitter build with Proc Macros.', href: 'https://github.com/jonas089/raptor-abi', icon: github },
-  { name: 'Trike-shield', description: 'Secure communication in Python with Diffie-Hellman and AES.', href: 'https://github.com/jonas089/trike-shield', icon: github },
-  { name: 'React-Voting', description: 'Voting App built on the Casper Blockchain with Casper Wallet support.', href: 'https://github.com/jonas089/casper-voting-dapp-demo', icon: github },
-  { name: 'ECDSA-Noir', description: 'Input generator for Noir zk circuits that utilize ECDSA (secp256k1) written in Rust.', href: 'https://github.com/jonas089/ecdsa-circuit-input-lib', icon: github },
-  { name: 'Merkle Trees', description: 'Efficient Rust Merkle Tree(s) for use in Blockchain systems.', href: 'https://github.com/jonas089/thyla-trees/blob/master/merkle-tree/src/tornado.rs', icon: github },
-  { name: 'ECDSA', description: 'Elliptic Curve digital Signatures from scratch in Python.', href: 'https://github.com/jonas089/compy-sign', icon: github },
-  { name: 'Inpigritas', description: 'A blockchain transaction network built from scratch in Python (Flask, Pycryptodome).', href: 'https://github.com/jonas089/Inpigritas-2022', icon: github },
+const articles = [
   { name: 'Automated Market Maker', description: 'Automated Market Maker Smart Contracts written in Rust.', href: 'https://medium.com/casperblockchain/building-anautomated-market-maker-on-casper-fd35c5fa917e', icon: medium },
   { name: 'R&D Blog', description: 'Articles on Cryptography, Blockchain and Engineering.', href: 'https://medium.com/@jonas_casper', icon: medium },
 ]
@@ -57,13 +46,15 @@ class Home extends React.Component {
       <div>
         <div className=''>
           <div className="flex flex-col items-center justify-center bg-black">
+            {/* 
+              Candidate information and Links
+            */}
             <h1 className="pb-10 pt-10 flex items-center text-5xl font-bold bg-gradient-to-r from-emerald-500 to-blue-500 text-transparent bg-clip-text">
               Jonas Pauli
-              
             </h1>
             <div className='px-5 flex flex-col items-center justify-center flex-auto overflow-hidden rounded-3xl text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 bg-gradient-to-r from-emerald-500 to-blue-500 w-3/4'>
               <br />
-              <p className='text-xl font-bold'>Experienced Rust Engineer specialized in Systems & Cryptography Engineering</p>
+              <p className='text-xl font-bold'>Experienced Rust Engineer specialized in Systems Design & Cryptography</p>
               <br />
               <p>Current occupation: R&D Engineer at Casper Association</p>
               <br />
@@ -72,25 +63,46 @@ class Home extends React.Component {
                 <a href='https://www.linkedin.com/in/jonas-pauli/' className='ml-4 border border-black text-black rounded-md px-10 py-2 transition-colors duration-300 hover:bg-blue-500 hover:text-black'>LinkedIn</a>
               </div>      
             </div>
-
-
             <GitHubProfile/>
+            {/* 
+              Medium articles - R&D
+            */}
+            <h1 className="pb-10 pt-10 flex items-center text-5xl font-bold bg-gradient-to-r from-emerald-500 to-blue-500 text-transparent bg-clip-text">
+              Articles
+            </h1>
+            <div className='flex flex-col items-center justify-center bg-black w-3/4'>
+              <ul className="list-none w-full">
+                {articles.map((article, index) => (
+                      <li key={index} className="mb-4">
+                          <a href={article.href} className="block bg-gradient-to-r from-emerald-500 to-blue-500 rounded-lg shadow-md p-4">
+                              <h1 style={{ fontFamily: "'Roboto Slab', cursive", textTransform: "uppercase" }} className='text-black text-xl'>{article.name}</h1>
+                              <p className='text-gray-700'>{article.description}</p>
+                          </a>
+                      </li>
+                  ))}
+              </ul>
+            </div>
 
+            {/* 
+              Public documents & downloads - includes the CV as PDF.
+            */}
             <h1 className="pb-10 pt-10 flex items-center text-5xl font-bold bg-gradient-to-r from-emerald-500 to-blue-500 text-transparent bg-clip-text">
               Documents
             </h1>
-    
-              <div className='flex flex-col items-center justify-center bg-gradient-to-r from-emerald-500 to-blue-500 rounded-lg shadow-md p-4 mb-4 w-3/4'>
-                <a className='py-3 px-3 border border-emerald-900 rounded-md hover:bg-blue-500' href="/cv.pdf" download="cv.pdf">Download CV</a> 
-              </div>
-    
+            <div className='flex flex-col items-center justify-center bg-gradient-to-r from-emerald-500 to-blue-500 rounded-lg shadow-md p-4 mb-4 w-3/4'>
+              <a className='py-3 px-3 border border-emerald-900 rounded-md hover:bg-blue-500' href="/jonas-pauli-cv.pdf" download="jonas-pauli-cv.pdf">Download CV</a> 
+            </div>
 
           </div>
         </div>
 
+
+        {/* 
+          Footer and contacts
+        */}
         <footer className="bg-black">
           <div className="mx-auto max-w-7xl overflow-hidden px-6 py sm:py lg:px-8">
-            <div className="mt-10 flex justify-center space-x-10">
+            <div className="mt-10 pb-10 flex justify-center space-x-10">
               {navigation.social.map((item) => (
                 <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
                   <span className="sr-only">{item.name}</span>
@@ -98,9 +110,6 @@ class Home extends React.Component {
                 </a>
               ))}
             </div>
-            <p className="mt-10 text-center text-20 leading-5 text-white">
-              last updated: 13.12.2023
-            </p>
           </div>
         </footer>
 
--- src/services/Github.jsx
+++ src/services/Github.jsx
@@ -1,6 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const GitHubActivity = () => {
+    const username = 'jonas089';
+    const contributionImageUrl = `https://ghchart.rshah.org/${username}`;
+  
+    return (
+    <div className='pt-10 pb-10 flex flex-col items-center justify-center'>
+        <img src={contributionImageUrl} alt="GitHub Contributions" />
+        <p style={{ 
+            fontFamily: "'Roboto Slab', serif", // Use serif instead of cursive for Roboto Slab
+            fontSize: '1.2em', // Larger font size
+            fontWeight: 'bold', // Make it bold
+            textAlign: 'center' // Center align the text if needed
+            }} 
+            className='text-gray-700'>
+            Activity Monitor
+        </p>
+    </div>
+    );
+};
+
 const GitHubProfile = () => {
     const [profile, setProfile] = useState(null);
     const [Repos, setRepos] = useState(null);
@@ -19,8 +39,8 @@ const GitHubProfile = () => {
                 setRepos(response.data);
                 const reposFilteredByTag = response.data.filter(repo => 
                     {
-                    console.log(repo.topics);
-                    return repo.topics.includes('portfolio')
+                        console.log(repo.topics);
+                        return repo.topics.includes('portfolio')
                     }
                 );
                 setFilteredRepos(reposFilteredByTag);
@@ -40,16 +60,22 @@ const GitHubProfile = () => {
               Github
               <img src={profile.avatar_url} alt="Profile" className="w-30 h-16 ml-10 rounded-full" />
             </h1>
-            <h1>{profile.name}</h1>
-            <p>{profile.bio}</p>            
+            <GitHubActivity/>           
             <ul className="list-none w-full">
-                <p className='text-white'>Public Repositories: {profile.public_repos}</p>
+                <p style={{ fontFamily: "'Roboto Slab', cursive" }} className='text-white'>Public Repositories: {profile.public_repos}</p>
                 {filteredRepos.map((repo, index) => (
-                    <li key={index} className="bg-gradient-to-r from-emerald-500 to-blue-500 rounded-lg shadow-md p-4 mb-4">
-                        <h1 className='text-blue-900 text-xl'><a href={repo.html_url}>{repo.name}</a></h1>
-                        <p className='text-black'>{repo.description}</p>
+                    <li key={index} className="mb-4">
+                        <a href={repo.html_url} className="block bg-gradient-to-r from-emerald-500 to-blue-500 rounded-lg shadow-md p-4">
+                            <h1 style={{ fontFamily: "'Roboto Slab', cursive", textTransform: "uppercase" }} className='text-black text-xl'>{repo.name}</h1>
+                            <p className='text-gray-700'>{repo.description}</p>
+                        </a>
                     </li>
                 ))}
+                <li className="mb-4">
+                    <a href='https://github.com/jonas089?tab=repositories' className="block bg-gradient-to-r from-emerald-500 to-blue-500 rounded-lg shadow-md p-4">
+                        <h1 className='text-blue-900 text-xl'>...</h1>
+                    </a>
+                </li>
             </ul>
 
         </div>
